af9072070becce0468eb1346b9b01e8f5e967f30
Yifu Zhang <953260160@qq.com>
2020-05-03 10:08:27 +0800

    errors can be seen in demo.py

--- src/demo.py
+++ src/demo.py
@@ -27,11 +27,7 @@ def demo(opt):
     frame_rate = dataloader.frame_rate
 
     frame_dir = None if opt.output_format == 'text' else osp.join(result_root, 'frame')
-    try:
-        eval_seq(opt, dataloader, 'mot', result_filename,
-                 save_dir=frame_dir, show_image=False, frame_rate=frame_rate)
-    except Exception as e:
-        logger.info(e)
+    eval_seq(opt, dataloader, 'mot', result_filename, save_dir=frame_dir, show_image=False, frame_rate=frame_rate)
 
     if opt.output_format == 'video':
         output_video_path = osp.join(result_root, 'result.mp4')
