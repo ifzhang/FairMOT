bcc0e3f9812e8ab75e855a58e1c0c5cd0138fe01
Yifu Zhang <953260160@qq.com>
2020-05-18 00:05:14 +0800

    change batchsize of dla34 training

--- experiments/all_dla34.sh
+++ experiments/all_dla34.sh
@@ -1,3 +1,3 @@
 cd src
-python train.py mot --exp_id all_dla34 --gpus 0,1 --batch_size 8 --load_model '../models/ctdet_coco_dla_2x.pth'
+python train.py mot --exp_id all_dla34 --gpus 0,1 --batch_size 12 --load_model '../models/ctdet_coco_dla_2x.pth'
 cd ..
