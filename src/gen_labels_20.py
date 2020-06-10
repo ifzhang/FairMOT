7464c6c7f0c949a7945471a0941c0a63b7748ae5
Florentijn <florentijn.degroote@ml6.eu>
2020-06-10 13:14:17 +0200

    change path from absoluto to relative and clone latest DCNv2

--- src/gen_labels_20.py
+++ src/gen_labels_20.py
@@ -7,9 +7,8 @@ def mkdirs(d):
     if not osp.exists(d):
         os.makedirs(d)
 
-
-seq_root = '/data/yfzhang/MOT/JDE/MOT20/images/train'
-label_root = '/data/yfzhang/MOT/JDE/MOT20/labels_with_ids/train'
+seq_root = './data/MOT20/images/train'
+label_root = './data/MOT20/labels_with_ids/train'
 mkdirs(label_root)
 seqs = [s for s in os.listdir(seq_root)]
 
