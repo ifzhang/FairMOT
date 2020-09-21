b938d475ccd22ea63e2dd46a764fab4610173c74
Yifu Zhang <953260160@qq.com>
2020-09-21 20:43:19 +0800

    Update opts.py

--- src/lib/opts.py
+++ src/lib/opts.py
@@ -104,7 +104,7 @@ class opts(object):
     self.parser.add_argument('--test_mot15', default=False, help='test mot15')
     self.parser.add_argument('--val_mot16', default=False, help='val mot16 or mot15')
     self.parser.add_argument('--test_mot17', default=False, help='test mot17')
-    self.parser.add_argument('--val_mot17', default=True, help='val mot17')
+    self.parser.add_argument('--val_mot17', default=False, help='val mot17')
     self.parser.add_argument('--val_mot20', default=False, help='val mot20')
     self.parser.add_argument('--test_mot20', default=False, help='test mot20')
     self.parser.add_argument('--val_hie', default=False, help='val hie')
