9fc4fd2f61e6cc9b0c57f4b359a1128ce884997f
Yifu Zhang <953260160@qq.com>
2020-09-29 11:16:53 +0800

    change dir for crowdhuman

--- src/gen_labels_crowd.py
+++ src/gen_labels_crowd.py
@@ -52,9 +52,9 @@ if __name__ == '__main__':
     data_val = '/data/yfzhang/MOT/JDE/crowdhuman/images/val'
     label_val = '/data/yfzhang/MOT/JDE/crowdhuman/labels_with_ids/val'
     ann_val = '/data/yfzhang/MOT/JDE/crowdhuman/annotation_val.odgt'
-    data_train = '/data/yfzhang/MOT/JDE/crowdhuman/images/val'
-    label_train = '/data/yfzhang/MOT/JDE/crowdhuman/labels_with_ids/val'
-    ann_train = '/data/yfzhang/MOT/JDE/crowdhuman/annotation_val.odgt'
+    data_train = '/data/yfzhang/MOT/JDE/crowdhuman/images/train'
+    label_train = '/data/yfzhang/MOT/JDE/crowdhuman/labels_with_ids/train'
+    ann_train = '/data/yfzhang/MOT/JDE/crowdhuman/annotation_train.odgt'
     gen_labels_crowd(data_train, label_train, ann_train)
     gen_labels_crowd(data_val, label_val, ann_val)
 
