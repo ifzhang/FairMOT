40126bf26d2bd6cc43106a0c794768467c8f62fd
Callum Downie <callum@callumdownie.com>
2020-12-04 12:28:13 +0000

    add setup.py, add to .gitignore

---
+++ setup.py
@@ -0,0 +1,39 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="fairmot",
+    version="0.1.0",
+    author="Yifu Zhang",
+    description="A simple baseline for one-shot multi-object tracking.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ifzhang/FairMOT",
+    packages=setuptools.find_packages(),
+    package_dir={"fairmot": "src/lib"},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        "cython",
+        "cython-bbox",
+        "matplotlib",
+        "motmetrics",
+        "numba",
+        "opencv-python",
+        "openpyxl",
+        "Pillow",
+        "lap",
+        "progress",
+        "scipy",
+        "torch>=1.2.0",
+        "torchvision>=0.4.0",
+        "yacs",
+    ],
+    extras_require={"tensorboard": ["tensorboardX"]},
+)
