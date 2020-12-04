6a3656363d4b13fe8364b8e017583dc7678fa58f
Callum Downie <callum@callumdownie.com>
2020-12-04 15:13:31 +0000

    replace find_packages

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/ifzhang/FairMOT",
-    packages=setuptools.find_packages(),
+    packages=["fairmot"],
     package_dir={"fairmot": "src/lib"},
     classifiers=[
         "Programming Language :: Python :: 3",
