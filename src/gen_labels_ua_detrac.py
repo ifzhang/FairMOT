d020d85db48c6375bdb4f0fbc480aa03ba407b98
HsiuWen <seawing1984@gmail.com>
2021-04-12 19:35:21 +0200

    add objects name to output in labels txt.

--- src/gen_labels_ua_detrac.py
+++ src/gen_labels_ua_detrac.py
@@ -14,14 +14,14 @@ def load_func(fpath):
     return records
 
 
-def gen_labels_uadetrac(data_root, label_root, ann_root):
+def gen_labels_uadetrac(data_root, label_root, ann_root, names):
     '''This is a function that read images (sequences) from data_root and annotation data from ann_root
        The generated label data (given in the path label_root) in txt files are the one used for train.py
        TODO: all gen_labels_*.py do not include the traking id information to generate txt under labels_with_ids. 
        TODO: there is no object type from groundtruth written to labels_with_ids txt. Should has this option in the future
     '''
     label_root.mkdir(parents = True, exist_ok = True)
-    
+
     for sequence_path in ann_root.iterdir():
         print('Processing sequence:'+sequence_path.name)
         tree = ET.parse(sequence_path)
@@ -50,16 +50,23 @@ def gen_labels_uadetrac(data_root, label_root, ann_root):
                     anns = target[0].attrib
                     # Control target objects by the conditions provided in target[1].attrib
                     # There are {orientation, speed, trajectory_length', 'truncation_ratio', 'vehicle_type'}
-                    #if target[1].attrib['vehicle_type'] is not 'car':
-                    #    continue
+                    if target[1].attrib['vehicle_type'] not in names:
+                        print('Error that target object is not in provided class name list !')
+                        break
+
                     x, y, w, h = float(anns['left']), float(anns['top']), float(anns['width']), float(anns['height'])
                     x += w / 2
                     y += h / 2
                     label_fpath = (label_root/sequence_path.stem/image_name).with_suffix('.txt')
                     
-                    label_str = '0 {:d} {:.6f} {:.6f} {:.6f} {:.6f}\n'.format(
-                        tid_curr, x / img_width, y / img_height, w / img_width, h / img_height)
+                    # Expected format: class id x_center/img_width y_center/img_height w/img_width h/img_height
+                    #label_str = '0 {:d} {:.6f} {:.6f} {:.6f} {:.6f}\n'.format(
+                    #    tid_curr, x / img_width, y / img_height, w / img_width, h / img_height)
                     
+                    # With the class id 
+                    label_str = '{:d} {:d} {:.6f} {:.6f} {:.6f} {:.6f}\n'.format(names.index(target[1].attrib['vehicle_type']),
+                        tid_curr, x / img_width, y / img_height, w / img_width, h / img_height)
+
                     if not label_fpath.parents[0].exists():
                         label_fpath.parents[0].mkdir(parents=True)
 
@@ -79,5 +86,15 @@ if __name__ == '__main__':
     label_val = root_path/'labels_with_ids/val'
     ann_val = root_path / 'DETRAC-Test-Annotations-XML'
 
-    gen_labels_uadetrac(path_images, label_train, ann_train)
-    gen_labels_uadetrac(path_images, label_val, ann_val)
+    # New classes name for CVAT label.txt
+    names = list(('car', 'bus', 'van','others'))
+    cvat_label_path = root_path/'for_cvat'/'labels.txt'
+    if not cvat_label_path.parents[0].exists():
+        cvat_label_path.parents[0].mkdir(parents=True)
+
+    with cvat_label_path.open('w') as out_txt_file:
+        for i in names:
+            print(i, file =out_txt_file )
+
+    gen_labels_uadetrac(path_images, label_train, ann_train, names)
+    gen_labels_uadetrac(path_images, label_val, ann_val,names)
