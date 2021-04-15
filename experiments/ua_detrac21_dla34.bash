2dc840aebfcddc829030186270bafd8c7828211b
HsiuWen <seawing1984@gmail.com>
2021-04-15 15:19:15 +0200

    delete extra files

--- experiments/ua_detrac21_dla34.bash
+++
@@ -1,3 +0,0 @@
-cd src
-python train.py mot --exp_id ua-detrac21_dla34 --batch_size 8 --gpus 0 --load_model '../models/fairmot_dla34.pth' --data_cfg 'lib/cfg/ua_detrac.json' --data_dir '../../data/UA-DETRAC'
-cd ..
