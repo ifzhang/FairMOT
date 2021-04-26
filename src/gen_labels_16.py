d7b5f6fd0f9aef0b95e2cebb2ece5294d6acb471
HsiuWen <seawing1984@gmail.com>
2021-04-26 13:54:46 +0200

    change file operation to pathlib and clean label_with_ids files before append

--- src/gen_labels_16.py
+++ src/gen_labels_16.py
@@ -1,30 +1,35 @@
 import os.path as osp
 import os
 import numpy as np
-
+from pathlib import Path
 
 def mkdirs(d):
     if not osp.exists(d):
         os.makedirs(d)
 
 
-seq_root = '/data/yfzhang/MOT/JDE/MOT16/images/train'
-label_root = '/data/yfzhang/MOT/JDE/MOT16/labels_with_ids/train'
-mkdirs(label_root)
-seqs = [s for s in os.listdir(seq_root)]
+seq_root = Path('../../MOT_data/MOT17/images/train')
+label_root = Path('../../MOT_data/MOT17/labels_with_ids/train')
+label_root.mkdir(parents=True, exist_ok = True)
+seqs = [s for s in seq_root.iterdir()]
 
 tid_curr = 0
 tid_last = -1
+
+# clear label txt files before append 
+for gt_files in label_root.glob('**/**/*.txt'):
+    gt_files.unlink()
+
 for seq in seqs:
-    seq_info = open(osp.join(seq_root, seq, 'seqinfo.ini')).read()
+    seq_info = open(seq/'seqinfo.ini').read()
     seq_width = int(seq_info[seq_info.find('imWidth=') + 8:seq_info.find('\nimHeight')])
     seq_height = int(seq_info[seq_info.find('imHeight=') + 9:seq_info.find('\nimExt')])
 
-    gt_txt = osp.join(seq_root, seq, 'gt', 'gt.txt')
+    gt_txt = seq/'gt'/'gt.txt'
     gt = np.loadtxt(gt_txt, dtype=np.float64, delimiter=',')
 
-    seq_label_root = osp.join(label_root, seq, 'img1')
-    mkdirs(seq_label_root)
+    seq_label_root = label_root/seq.stem/'img1'
+    seq_label_root.mkdir(parents=True, exist_ok = True)
 
     for fid, tid, x, y, w, h, mark, label, _ in gt:
         if mark == 0 or not label == 1:
@@ -36,8 +41,8 @@ for seq in seqs:
             tid_last = tid
         x += w / 2
         y += h / 2
-        label_fpath = osp.join(seq_label_root, '{:06d}.txt'.format(fid))
+        label_fpath = seq_label_root/'{:06d}.txt'.format(fid)
         label_str = '0 {:d} {:.6f} {:.6f} {:.6f} {:.6f}\n'.format(
             tid_curr, x / seq_width, y / seq_height, w / seq_width, h / seq_height)
-        with open(label_fpath, 'a') as f:
+        with label_fpath.open('a') as f:
             f.write(label_str)
